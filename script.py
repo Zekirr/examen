bed7a04e64cd452a20bc542f2fb3ab5a6c7618f0
Zekirr <59977659+Zekirr@users.noreply.github.com>
2020-03-09 14:23:35 +0100

    hotfix bruh

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 print("Hello World")
 
-Q1= input("Comment vas tu ?")
+Q1 = input("Comment vas tu ?")
 if Q1 == "bien":
     print("cool")
 else:
@@ -13,7 +13,30 @@ else:
     print("Faux")
 
 Q3 = input("La couleur du cheval blanc d'Henry 4? ")
-if Q3 == "blanc"
+if Q3 == "blanc":
     print("Bien vu")
 else:
     print("non")
+    
+print("Good bye !")
+print("et je suis de retour")
+
+Q4 = input("content de me revoir? ")
+if Q4 == "yes":
+    print("tant mieux")
+else: 
+    print("tant pis")
+    
+Q5 = input("60/2? ")
+if q5 == "30":
+    print("bien jouée")
+else:
+    print(faux)
+    
+Q6 = input("150/3 ?")
+if Q6 == "50":
+    print("tu es fort en math")
+else:
+    print("pas ouf")
+    
+print("bruh")
