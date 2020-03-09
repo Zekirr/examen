98465b2efbc50a14ad5a9c567571b97a3804b5f9
Zekirr <59977659+Zekirr@users.noreply.github.com>
2020-03-09 13:52:03 +0100

    Script HW+Q1+Q2+Q3

--- script.py
+++ script.py
@@ -1,14 +1,19 @@
 print("Hello World")
-Q1= input("Comment vas tu ?")
 
+Q1= input("Comment vas tu ?")
 if Q1 == "bien":
     print("cool")
 else:
     print("dommage")
     
 Q2 = input("2+2?")
-
 if Q2 == "4":
     print("Tu sais compter bravo")
 else:
     print("Faux")
+
+Q3 = input("La couleur du cheval blanc d'Henry 4? ")
+if Q3 == "blanc"
+    print("Bien vu")
+else:
+    print("non")
